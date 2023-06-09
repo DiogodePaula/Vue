e09ca327a32eaadbd3c6e9bda4cbd01da7a332c1
LAPTOP\Diogo <depaulatrabalhos@gmail.com>
2023-06-09 18:13:33 -0300

    ..

--- Vue3 avançando no framework/src/router/index.ts
+++ Vue3 avançando no framework/src/router/index.ts
@@ -2,6 +2,8 @@ import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
 
 import Tarefa from "@/views/Tarefa.vue";
 import Projetos from "@/views/Projetos.vue";
+import Formulario from "@/views/Projetos/Formulario.vue";
+import Lista from "@/views/Projetos/Lista.vue";
 
 const rotas: RouteRecordRaw[] = [
 	{
@@ -11,8 +13,25 @@ const rotas: RouteRecordRaw[] = [
 	},
 	{
 		path: "/projetos",
-		name: "Projetos",
 		component: Projetos,
+		children: [
+			{
+				path: "",
+				name: "Projetos",
+				component: Lista,
+			},
+			{
+				path: "novo",
+				name: "Novo projetos",
+				component: Formulario,
+			},
+			{
+				path: ":id",
+				name: "Editar projetos",
+				component: Formulario,
+				props: true, // vai injetar o parâmetro da url como uma propriedade no componente
+			},
+		],
 	},
 ];
 
--- Vue3 avançando no framework/src/store/index.ts
+++ Vue3 avançando no framework/src/store/index.ts
@@ -1,7 +1,8 @@
 import { InjectionKey } from "vue";
-import { Store, createStore } from "vuex";
+import { Store, createStore, useStore as vuexUseStore } from "vuex";
 // @ é um apelido para src
 import IProjeto from "@/interfaces/IProjeto";
+import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO } from "./tipo-mutacoes";
 
 interface Estado {
 	projetos: IProjeto[];
@@ -12,19 +13,27 @@ export const key: InjectionKey<Store<Estado>> = Symbol();
 export const store = createStore<Estado>({
 	// configuração do estado
 	state: {
-		projetos: [
-			{
+		projetos: [],
+	},
+	mutations: {
+		[ADICIONA_PROJETO](state, nomeDoProjeto: string) {
+			const projeto = {
 				id: new Date().toISOString(),
-				nome: "Typescript",
-			},
-			{
-				id: new Date().toISOString(),
-				nome: "Vue",
-			},
-			{
-				id: new Date().toISOString(),
-				nome: "Vuex",
-			},
-		],
+				nome: nomeDoProjeto,
+			} as IProjeto;
+
+			state.projetos.push(projeto);
+		},
+		[ALTERA_PROJETO](state, projeto: IProjeto) {
+			const index = state.projetos.findIndex((proj) => proj.id == projeto.id);
+			state.projetos[index] = projeto;
+		},
+		[EXCLUIR_PROJETO](state, id: string) {
+			state.projetos = state.projetos.filter((proj) => proj.id != id);
+		},
 	},
 });
+
+export function useStore(): Store<Estado> {
+	return vuexUseStore(key);
+}
